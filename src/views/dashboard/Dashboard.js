5e0374654f93c76b86550f3826559432338dc289
Pasindu Yeshan Abeysinghe <pasindua.18@cse.mrt.ac.lk>
2022-08-08 22:19:57 +0530

    Components and dashboard

---
+++ src/views/dashboard/Dashboard.js
@@ -0,0 +1,62 @@
+import React, { lazy } from 'react'
+
+import {
+  CAvatar,
+  CButton,
+  CButtonGroup,
+  CCard,
+  CCardBody,
+  CCardFooter,
+  CCardHeader,
+  CCol,
+  CProgress,
+  CRow,
+  CTable,
+  CTableBody,
+  CTableDataCell,
+  CTableHead,
+  CTableHeaderCell,
+  CTableRow,
+} from '@coreui/react'
+import { CChartLine } from '@coreui/react-chartjs'
+import { getStyle, hexToRgba } from '@coreui/utils'
+import CIcon from '@coreui/icons-react'
+import {
+  cibCcAmex,
+  cibCcApplePay,
+  cibCcMastercard,
+  cibCcPaypal,
+  cibCcStripe,
+  cibCcVisa,
+  cibGoogle,
+  cibFacebook,
+  cibLinkedin,
+  cifBr,
+  cifEs,
+  cifFr,
+  cifIn,
+  cifPl,
+  cifUs,
+  cibTwitter,
+  cilCloudDownload,
+  cilPeople,
+  cilUser,
+  cilUserFemale,
+} from '@coreui/icons'
+
+const Dashboard = () => {
+
+
+  return (
+    <>
+      <CCard className="py-16 h-full">
+        <CCardBody>
+          
+        </CCardBody>
+        
+      </CCard> 
+    </>
+  )
+}
+
+export default Dashboard
