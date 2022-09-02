51a51624fa97a9d69db33e4a8bddf5d4e170e323
PasinduYeshan <pasindua.18@cse.mrt.ac.lk>
2022-09-02 21:26:22 +0530

    Admin, add , update, remove model

--- src/models/user.js
+++ src/models/user.js
@@ -14,11 +14,49 @@ const collectionName = "User";
 // Add user to the User collection
 export const addUser = async (user) => {
   const userRef = doc(collection(firestore, collectionName));
-  return await setDoc(userRef, { ...user, userID: userRef.id });
+  return await setDoc(userRef, {
+    ...user,
+    userID: userRef.id,
+    createdAt: new Date(),
+    isDeleted: false,
+  });
 };
 
-// Update user from the user collection
+// Update user from the User collection
 export const updateUser = async (userID, user) => {
   const userRef = doc(firestore, collectionName, userID);
-  return await setDoc(userRef, user);
+  return await updateDoc(userRef, user);
 };
+
+// Get all the user from user collection where the user is not deleted
+export const getAllUsers = async () => {
+  const q = query(
+    collection(firestore, collectionName),
+    where("isDeleted", "!=", true), where("role", "==", "Admin")
+  );
+  const querySnapshot = await getDocs(q);
+  let agents = [];
+  querySnapshot.forEach((doc) => {
+    agents.push(doc.data());
+  });
+  return agents;
+};
+
+// Get single user account by userID
+export const getUserByID = async (userID) => {
+  const userRef = doc(firestore, collectionName, userID);
+  const docSnap = await getDoc(userRef);
+  if (docSnap.exists()) {
+    return docSnap.data();
+  } else {
+    return null;
+  }
+}
+
+// Delete user account by userID
+export const deleteUser = async (userID) => {
+  const userRef = doc(firestore, collectionName, userID);
+  return await updateDoc(userRef, {
+    isDeleted: true,
+  });
+}
